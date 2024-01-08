38b96c492bbc4f3820ad7d5d1537c1774904ac89
tobias <tobias.tovedal@gmail.com>
2024-01-08 09:46:04 +0100

    What's this

---
+++ target/debug/deps/toggl_cli-a5a24b68c240eade.d
@@ -0,0 +1,5 @@
+/Users/tobias/Rust projects/toggl-cli/target/debug/deps/libtoggl_cli-a5a24b68c240eade.rmeta: src/main.rs
+
+/Users/tobias/Rust projects/toggl-cli/target/debug/deps/toggl_cli-a5a24b68c240eade.d: src/main.rs
+
+src/main.rs:
