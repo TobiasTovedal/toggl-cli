7d987f0a247d5b043700ab54e1c6650e05411d09
tobias <tobias.tovedal@gmail.com>
2024-01-09 12:23:35 +0100

    Create and import access-token module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use curl::easy::{Easy, self};
+mod access_token;
 
 fn main() {
     let mut easy = Easy::new();
 
-    println!("Hello, world!");
+    println!("Hello, worlsd!");
 }
--- target/debug/deps/toggl_cli-31b821ec5825d026.d
+++ target/debug/deps/toggl_cli-31b821ec5825d026.d
@@ -1,5 +1,6 @@
-/Users/tobias/Rust projects/toggl-cli/target/debug/deps/libtoggl_cli-31b821ec5825d026.rmeta: src/main.rs
+/Users/tobias/Rust projects/toggl-cli/target/debug/deps/libtoggl_cli-31b821ec5825d026.rmeta: src/main.rs src/access_token.rs
 
-/Users/tobias/Rust projects/toggl-cli/target/debug/deps/toggl_cli-31b821ec5825d026.d: src/main.rs
+/Users/tobias/Rust projects/toggl-cli/target/debug/deps/toggl_cli-31b821ec5825d026.d: src/main.rs src/access_token.rs
 
 src/main.rs:
+src/access_token.rs:
