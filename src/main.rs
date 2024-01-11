15ff149096b08f4930b56537f5a0f6e0644fa221
tobias <tobias.tovedal@gmail.com>
2024-01-11 12:25:56 +0100

    Successful API connection and auth

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+extern crate tokio;
+extern crate serde_json;
 mod access_token;
 
-fn main() {
-    //access_token::print_access_token();
-    print!("{}", access_token::get_access_token())
+#[tokio::main]
+async fn main() {
+    let client = reqwest::Client::new();
+
+    // Get information on current user?
+    let result = client.get("https://api.track.toggl.com/api/v9/me").basic_auth(access_token::get_access_token(), Some("api_token")).send().await.unwrap().text().await.unwrap();
+
+    println!("{:?}", result);
 }
