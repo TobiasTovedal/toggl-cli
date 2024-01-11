fd0ebe7c44884c51db98d4a960243094d2165085
tobias <tobias.tovedal@gmail.com>
2024-01-11 14:22:51 +0100

    Set header type to application/json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use reqwest::header::CONTENT_TYPE;
+
 extern crate tokio;
 extern crate serde_json;
 mod access_token;
@@ -7,12 +9,18 @@ async fn main() {
     let client = reqwest::Client::new();
 
     // Get information on current user?
-    let response = client.get("https://api.track.toggl.com/api/v9/me").basic_auth(access_token::get_access_token(), Some("api_token")).send().await.unwrap();
+    let response = client.get("https://api.track.toggl.com/api/v9/me")
+        .basic_auth(access_token::get_access_token(), Some("api_token"))
+        .header(CONTENT_TYPE, "application/jsaon")
+        .send()
+        .await
+        .unwrap();
 
-    // Different behaviours depending on response status. Possible statuses from Toggl API: 200, 403, 404, 500.
+    // Different behaviours depending on response status.
+    // TODO: Implement cases for all status code responses according to Toggl API reference: https://developers.track.toggl.com/docs/#generic-responses
     match response.status() {
         reqwest::StatusCode::OK => {
-            println!("ok");
+            println!("ok {:?}", response.text().await);
         }
         reqwest::StatusCode::FORBIDDEN => {
             println!("forbidden");
