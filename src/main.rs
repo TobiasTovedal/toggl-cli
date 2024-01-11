d52dcdf6c21057860fe558740db90e9ea486c0f1
tobias <tobias.tovedal@gmail.com>
2024-01-11 13:16:22 +0100

    Match for different status codes

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,24 @@ async fn main() {
     let client = reqwest::Client::new();
 
     // Get information on current user?
-    let result = client.get("https://api.track.toggl.com/api/v9/me").basic_auth(access_token::get_access_token(), Some("api_token")).send().await.unwrap().text().await.unwrap();
+    let response = client.get("https://api.track.toggl.com/api/v9/me").basic_auth(access_token::get_access_token(), Some("api_token")).send().await.unwrap();
 
-    println!("{:?}", result);
+    // Different behaviours depending on response status. Possible statuses from Toggl API: 200, 403, 404, 500.
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            println!("ok");
+        }
+        reqwest::StatusCode::FORBIDDEN => {
+            println!("forbidden");
+        }
+        reqwest::StatusCode::NOT_FOUND => {
+            println!("not found");
+        }
+        reqwest::StatusCode::INTERNAL_SERVER_ERROR => {
+            println!("internal server error");
+        }
+        _ => {
+            println!("oh god, no");
+        }
+    }
 }
