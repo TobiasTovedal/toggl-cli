1a3c5c4c1049ad4012dde1b2b5a2fd15b0510f43
tobias <tobias.tovedal@gmail.com>
2024-01-14 21:40:00 +0100

    Current user to json + drop status code check

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
 use reqwest::header::CONTENT_TYPE;
+use serde::{Deserialize, Serialize};
 
 extern crate tokio;
 extern crate serde_json;
 mod access_token;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    email: String,
+    api_token: String,
+}
+
 #[tokio::main]
 async fn main() {
     let client = reqwest::Client::new();
 
-    // Get information on current user?
-    let response = client.get("https://api.track.toggl.com/api/v9/me")
+    // Get information on current user?    
+    let response: Person = client.get("https://api.track.toggl.com/api/v9/me")
         .basic_auth(access_token::get_access_token(), Some("api_token"))
-        .header(CONTENT_TYPE, "application/jsaon")
+        .header(CONTENT_TYPE, "application/json")
         .send()
+        .await
+        .unwrap()
+        .json()
         .await
         .unwrap();
 
+    print!("{:?}", response)
+
+
     // Different behaviours depending on response status.
     // TODO: Implement cases for all status code responses according to Toggl API reference: https://developers.track.toggl.com/docs/#generic-responses
+    /* 
     match response.status() {
         reqwest::StatusCode::OK => {
-            println!("ok {:?}", response.text().await);
+            println!("ok {:?}", response.);
         }
         reqwest::StatusCode::FORBIDDEN => {
             println!("forbidden");
@@ -35,4 +49,6 @@ async fn main() {
             println!("oh god, no");
         }
     }
+    */
+    
 }
