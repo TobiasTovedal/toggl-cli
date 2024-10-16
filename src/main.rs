dfddd70f066efc54dc64ea17636f633533bbaa8a
TobiasTovedal <tobias.tovedal@gmail.com>
2024-10-16 14:13:46 +0200

    Remove unused TimeEntryResponse struct

--- src/main.rs
+++ src/main.rs
@@ -32,41 +32,6 @@ struct Person {
     api_token: String,
 }
 
-// Placeholder struct for return variable from POST time entry
-#[derive(Debug, Serialize, Deserialize)]
-struct TimeEntryResponse {
-    at: String,
-    billable: bool,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    description: Option<String>,
-    duration: i64,
-    duronly: bool,
-    id: i64,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pid: Option<i64>,  // Project ID, legacy field
-    #[serde(skip_serializing_if = "Option::is_none")]
-    project_id: Option<i64>,  // Project ID. Can be null if project was not provided or project was later deleted
-    #[serde(skip_serializing_if = "Option::is_none")]
-    server_deleted_at: Option<String>,  // When was deleted, null if not deleted
-    start: String,  // Start time in UTC
-    #[serde(skip_serializing_if = "Option::is_none")]
-    stop: Option<String>,  // Stop time in UTC, can be null if it's still running or created with "duration" and "duronly" fields
-    #[serde(skip_serializing_if = "Option::is_none")]
-    tag_ids: Option<Vec<i64>>,  // Array of integer Tag IDs, null if tags were not provided or were later deleted
-    #[serde(skip_serializing_if = "Option::is_none")]
-    tags: Option<Vec<String>>,  // Array of string Tag names, null if tags were not provided or were later deleted
-    #[serde(skip_serializing_if = "Option::is_none")]
-    task_id: Option<i64>,  // Task ID. Can be null if task was not provided or project was later deleted
-    #[serde(skip_serializing_if = "Option::is_none")]
-    tid: Option<i64>,  // Task ID, legacy field
-    #[serde(skip_serializing_if = "Option::is_none")]
-    uid: Option<i64>,  // Time Entry creator ID, legacy field
-    user_id: i64,  // Time Entry creator ID
-    #[serde(skip_serializing_if = "Option::is_none")]
-    wid: Option<i64>,  // Workspace ID, legacy field
-    workspace_id: i64,  // Workspace ID
-}
-
 #[tokio::main]
 async fn main() {
     // Parse input arguments
